a8d1ed1cb5eb7d320c95b591a684ba343585ed0f
Firstname Lastname <carlos.oliveros@koombea.com>
2011-10-07 18:28:32 -0500

    Has graphic en frontend and index home in frontend

--- Gemfile
+++ Gemfile
@@ -10,6 +10,13 @@ gem 'bcrypt-ruby' ,'2.1.3'
 gem 'warden', '1.0.3'
 gem 'devise' ,'1.1.5'
 gem 'paperclip','2.4.3'
+#gem 'scruffy','0.2.6'
+#gem "gruff", "~> 0.3.6"
+#gem "rmagick", "~> 2.13.1"
+#gem 'hoe', '1.12.1' 
+gem "google_visualr", "~> 2.1.0" 
+
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,19 @@
 class HomeController < ApplicationController
+
   def index
     if user_signed_in?
       redirect_to profile_path
+    else
+      @performances = Performance.last(10)
+      @cheapest_cost_gal = Performance.first(:conditions => {:cost_gallon => Performance.minimum(:cost_gallon)})
+      @best_km_gallon = Performance.first(:conditions => {:km_gallon => Performance.maximum(:km_gallon)})
+      most_vehil_performances =  Performance.all
+      @most_vehil = most_vehil_performances.first
+      most_vehil_performances.each do |performance|
+       if (performance.cost_gallon/performance.km_gallon) < (@most_vehil.cost_gallon/ @most_vehil.km_gallon) 
+         @most_vehil = performance
+       end
+      end
     end
   end
 end
