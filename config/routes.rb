dfc3b8ec817f388aa2df181f21601e7f732986fe
Firstname Lastname <carlos.oliveros@koombea.com>
2011-10-08 04:48:54 -0500

    Has almost finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Gasolineapp::Application.routes.draw do
-  resources :performances, :only=>[:index, :create]
+  resources :performances, :only=>[:create]
   root :to => "home#index"
   devise_for :users, controllers: {registrations: "users/registrations"}
   resource :profile , only: [:show,:edit,:update], controller: "users/profiles"
   resource :car , only: [:show,:edit,:update]
   resource :performance, only: [:new]
+  match 'station/:station' => 'performances#index', :as => 'station'
+  match 'user/:user' => 'performances#index', :as => 'user'
+  match 'car_info/:car' => 'performances#index', :as => 'car_info'
 end
